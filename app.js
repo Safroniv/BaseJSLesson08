89b369ed6f4eefe14e6a472397d5900a2c7965ea
Иван Сафронов <safroniv@mail.ru>
2022-11-27 14:37:04 +0300

    BaseJSLesson08 red002

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let fitlerPopup = document.querySelector('.filterPopup');
-let fitlerLabel = document.querySelector('.filterLabel');
-let filterIcon = document.querySelector('.filterIcon');
+const fitlerPopup = document.querySelector('.filterPopup');
+const fitlerLabel = document.querySelector('.filterLabel');
+const filterIcon = document.querySelector('.filterIcon');
 
-fitlerLabel.addEventListener('click', function() {
+fitlerLabel.addEventListener('click', () => {
     fitlerPopup.classList.toggle('hidden');
     fitlerLabel.classList.toggle('filterLabelPink');
     filterIcon.classList.toggle('filterIconPink');
@@ -17,14 +17,14 @@ fitlerLabel.addEventListener('click', function() {
 });
 
 let filterHeaders = document.querySelectorAll('.filterCategoryHeader');
-filterHeaders.forEach(function(header) {
-    header.addEventListener('click', function(event) {
+filterHeaders.forEach(header => {
+    header.addEventListener('click', event => {
         event.target.nextElementSibling.classList.toggle('hidden');
     })
 });
 
 let filterSizes = document.querySelector('.filterSizes');
 let filterSizeWrap = document.querySelector('.filterSizeWrap');
-filterSizeWrap.addEventListener('click', function() {
+filterSizeWrap.addEventListener('click', () => {
     filterSizes.classList.toggle('hidden');
 });
